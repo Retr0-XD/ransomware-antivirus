869b5933b84cfe55df35569cb60796373c5c3b43
Retr0-XD <88275564+Retr0-XD@users.noreply.github.com>
2022-09-03 20:00:15 +0530

    c# source
    phew .. one of the biggeest thing i've done...

---
+++ dogesan/Antivirus/Form1.Designer.cs
@@ -0,0 +1,31 @@
+﻿namespace Antivirus
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+       
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ dogesan/Antivirus/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Antivirus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private int viruses = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] search = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.*");
+            progressBar1.Maximum = search.Length;
+            foreach (string item in search)
+            {
+                try
+                {
+                    StreamReader stream = new StreamReader(item);
+                    string read = stream.ReadToEnd();
+                    string[] virus = new string[] { "SUBSCRIBE" , "LIKE" , "COMMENT" };
+                    foreach (string st in virus)
+                    {
+                        if (Regex.IsMatch(read, st))
+                        {
+                            viruses += 1;
+                            listBox1.Items.Add(item);
+                            button3.Show();
+                            label2.Text = "Detected : " + viruses.ToString();
+
+                        }
+                        progressBar1.Increment(1);
+                    }
+                }
+                catch
+                {
+                    string read = item;
+                    string[] virus = new string[] { "VIRUSES", "INFECTED", "HACKED" };
+                    foreach (string st in virus)
+                    {
+                        if (Regex.IsMatch(read, st))
+                        {
+                            viruses += 1;
+                            listBox1.Items.Add(item);
+                            button3.Show();
+                            label2.Text = "Detected : " + viruses.ToString();
+
+                        }
+                        progressBar1.Increment(1);
+
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            label1.Text = "Selected Location : " + folderBrowserDialog1.SelectedPath;
+            viruses = 0;
+            progressBar1.Value = 0;
+            listBox1.Items.Clear();
+            button2.Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string removex = listBox1.Text;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            File.Delete(this.listBox1.Text);
+            listBox1.Items.Clear();
+            MessageBox.Show("Selected Malware Successfully Removed!");
+
+        }
+    }
+}
